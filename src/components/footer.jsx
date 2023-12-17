0a3b89119ead17451749c0997079ad0b6714f6da
Kebrouh <oli1999@hotmail.ca>
2023-12-16 23:59:58 -0500

    feature-footer-icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,25 +1,25 @@
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 
+import { IoCall, IoMail } from "react-icons/io5";
+
 import './footer.css';
 
 const Accueil = () => {
 
     return (
         <div className="footer">
-            
-            FOOTER
 
             <div className="contact">
                 <h2>Information de contact</h2>
                 <ul>
                     <li>
+                        <IoCall />
                         <p>514 569-9968</p>
-                        <img src="./../../public/img/no-icon.png" alt="placeholder" />
                     </li>
                     <li>
+                        <IoMail />
                         <p>oli1999@hotmail.ca</p>
-                        <img src="./../../public/img/no-icon.png" alt="placeholder" />
                     </li>
                 </ul>
             </div>
@@ -29,19 +29,19 @@ const Accueil = () => {
                 <ul>
                     <li>
                         <a href="https://www.linkedin.com/in/olivier-daigneault/" target="_blank">Linkedn</a>
-                        <img src="./../../public/img/no-icon.png" alt="placeholder" />
+                        <img src="/icons8-linkedin.svg" alt="placeholder" />
                     </li>
                     <li>
                         <a href="https://www.artstation.com/olivierdaigneault8" target="_blank">ArtStation</a>
-                        <img src="./../../public/img/no-icon.png" alt="placeholder" />
+                        <img src="/icons8-artstation.svg" alt="placeholder" />
                     </li>
                     <li>
                         <a href="https://www.behance.net/olivierdaignea1" target="_blank">Behance</a>
-                        <img src="./../../public/img/no-icon.png" alt="placeholder" />
+                        <img src="/icons8-behance.svg" alt="placeholder" />
                     </li>
                     <li>
                         <a href="https://github.com/Kebrouh" target="_blank">GitHub</a>
-                        <img src="./../../public/img/no-icon.png" alt="placeholder" />  
+                        <img src="/icons8-github.svg" alt="placeholder" />  
                     </li>
                 </ul>
             </div>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '/portfolio/',
   plugins: [react()],
+  build: {
+    // Adjust the include option based on your file structure
+    // For example, if your JSX files are in the "src" directory:
+    include: 'src/**/*.jsx',
+  },
 })
