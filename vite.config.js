3184a5e96f50c2490f0f4e84ad6d53797b7a28da
Kebrouh <oli1999@hotmail.ca>
2023-12-18 22:58:29 -0500

    feature-backInTime-beforeGhPagesTry

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/portfolio/',
+  base: '/portfolio/',
   plugins: [react()],
   build: {
     // Adjust the include option based on your file structure
     // For example, if your JSX files are in the "src" directory:
-    include: 'src/**/*.jsx',
+    // include: 'src/**/*.jsx',
+    assetsDir: 'assets',
+    outDir: 'build',
   },
 })
