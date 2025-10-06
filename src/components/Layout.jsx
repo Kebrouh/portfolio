2b51b393bc6573c3c41035ad43a14f8df21a4d07
Kebrouh <oli1999@hotmail.ca>
2025-10-05 21:22:03 -0400

    feature-global-animatedWavesBg

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,28 @@
 import Header from "./header";
 import Footer from "./footer";
+import Waves from './Waves';    //test
 
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
-        <div>
+
+        <div className="layout" >
+
+            <Waves
+                // lineColor="rgba(38, 39, 44, 1)"
+                lineColor="rgba(47, 48, 53, 1)"
+                backgroundColor="#2A2B30"
+                waveSpeedX={0.05}
+                waveSpeedY={0.01}
+                waveAmpX={40}
+                waveAmpY={20}
+                friction={0.9}
+                tension={0.01}
+                maxCursorMove={120}
+                xGap={20}
+                yGap={36}
+            />
 
             <div>
                 <Header />
