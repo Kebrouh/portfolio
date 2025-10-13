7efa49ae46e6becc8657ff82aadbfa5fa18c4368
Kebrouh <oli1999@hotmail.ca>
2025-10-13 14:28:42 -0400

    feature-footer-cvUpdatedVersion2025

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const DownloadCV = () => {
-  const pdfPathEn = '/Olivier_Daigneault_cv_EN.pdf';  // Update this with the actual path to your PDF file
-  const pdfPathFr = '/Olivier_Daigneault_cv_FR.pdf';  // Update this with the actual path to your PDF file
+  const pdfPathEn = '/Olivier_Daigneault_cv_EN_2025.pdf';  // Update this with the actual path to your PDF file
+  const pdfPathFr = '/Olivier_Daigneault_cv_FR_2025.pdf';  // Update this with the actual path to your PDF file
 
     return (
         <div className="cv">
             <p>CV - Olivier Daigneault :</p>
-            <a href={pdfPathEn} download="Olivier_Daigneault_cv_EN.pdf" target="_blank" rel="noopener noreferrer">
+            <a href={pdfPathEn} download="Olivier_Daigneault_cv_EN_2025.pdf" target="_blank" rel="noopener noreferrer">
                 En
             </a>
             <p>/</p>
-            <a href={pdfPathFr} download="Olivier_Daigneault_cv_FR.pdf" target="_blank" rel="noopener noreferrer">
+            <a href={pdfPathFr} download="Olivier_Daigneault_cv_FR_2025.pdf" target="_blank" rel="noopener noreferrer">
                 Fr
             </a>
         </div>
